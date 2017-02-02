6557c2d3139c7926eaa24141af67f9011a4d5ce8
Torey Hickman <torey@toreyhickman.com>
2017-02-02 16:06:23 -0500

    Remove explicit returns from route handler

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,11 +2,11 @@ post "/challenge-updates" do
   content_type :json
 
   unless from_github?
-    return halt(500, { message: "Signatures didn't match!" }.to_json)
+    halt(500, { message: "Signatures didn't match!" }.to_json)
   end
 
   unless merged_pull_request? && for_a_challenge?
-    return halt(422, { message: "Not for a merged pull request for a challenge repository." }.to_json)
+    halt(422, { message: "Not for a merged pull request for a challenge repository." }.to_json)
   end
 
   { message: "Thanks, GitHub.  Payload received." }.to_json
