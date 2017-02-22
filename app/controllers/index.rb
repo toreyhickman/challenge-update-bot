9e6d03a8d98920d98e9f36c9a39072e1b144b3dd
Torey Hickman <torey@toreyhickman.com>
2017-02-21 20:11:45 -0800

    Sends message to Slack

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,5 +13,15 @@ post "/challenge-updates" do
     halt(422, { message: "Not a merged pull request for a challenge repository." }.to_json)
   end
 
-  { message: "Thanks, GitHub.  Payload received." }.to_json
+  details_for_slack = SlackPullRequestParser.parse(event)
+
+  begin
+    client = Slack::Web::Client.new
+    client.chat_postMessage(details_for_slack)
+  rescue
+    halt(500, "Something went wrong when sending the message to Slack.")
+  end
+
+  { message: "Thanks, GitHub." }.to_json
 end
+
